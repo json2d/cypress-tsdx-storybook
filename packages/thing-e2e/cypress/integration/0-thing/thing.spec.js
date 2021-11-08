c60e4482071738df030ed8d78d4ef3ae2c647de2
Jason Yung <json.yung@gmail.com>
2021-11-08 17:31:01 -0500

    add integration test for default story
    checks for the text "snozzberries" in `div` with class `.thing`

---
+++ packages/thing-e2e/cypress/integration/0-thing/thing.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe("default storybook", () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit(
+      // this url targets storybook story directly, instead of through the storybook ui
+      "http://localhost:6006/iframe.html?id=welcome--default&args=&viewMode=story"
+    );
+  });
+
+  it("displays some text about snozzberries", () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get(".thing").contains("snozzberries");
+  });
+});
